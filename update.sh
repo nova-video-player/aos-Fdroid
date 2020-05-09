56ca9ef00767bcfb8d4a97a32adb66105bc89528
Marc de Courville <marc@courville.org>
2020-05-09 20:18:00 +0200

    tag contains vX.Y.Z cXYYYZZ for fdroid identification of versionName and codeName

--- update.sh
+++ update.sh
@@ -2,6 +2,18 @@
 
 set -ex
 version=$1
+
+vmajor=$(echo $version | cut -d. -f1)
+vminor=$(echo $version | cut -d. -f2)
+vpatch=$(echo $version | cut -d. -f3)
+
+# versionCode is XYYYZZ
+cmajor=$vmajor
+cminor=$(printf "%03d" $vminor)
+cpatch=$(printf "%02d" $vpatch)
+
+code=${cmajor}${cminor}${cpatch}
+
 rm -f manifest.xml
 wget https://github.com/nova-video-player/aos-AVP/releases/download/$version/manifest.xml
 for p in $(xmlstarlet sel -t -m //project -v ./@path -n manifest.xml);do
@@ -13,8 +25,8 @@ for p in $(xmlstarlet sel -t -m //project -v ./@path -n manifest.xml);do
     )
     git add $p
 done
-git commit -m $version
+git commit -m "v${version}"
 git push
-git tag -a "$version" -m "$version release"
+git tag -a "v${version} c${code}" -m "v${version} release"
 git push --tags
 
